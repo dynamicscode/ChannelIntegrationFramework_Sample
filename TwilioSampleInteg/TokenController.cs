e0eefeeb7cedb21a0f0176f03af4e9a04c421ec2
Aung Khaing <c.akhaing@gmail.com>
2019-03-22 00:03:21 +1300

    Initial commit

---
+++ TwilioSampleInteg/TokenController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Twilio.Jwt;
+using Twilio.Jwt.Client;
+using Twilio.TwiML;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TwilioSampleInteg
+{
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            // Put your Twilio API credentials here
+            const string accountSid = "";
+            const string authToken = "";
+
+            // Put your Twilio Application SID here
+            const string appSid = "";
+
+            var scopes = new HashSet<IScope>
+            {
+                new OutgoingClientScope(appSid),
+                new IncomingClientScope("ak")
+            };
+            var capability = new ClientCapability(accountSid, authToken, scopes: scopes);
+
+            return Content(capability.ToJwt(), "application/jwt");
+        }
+    }
+}
---
+++ TwilioSampleInteg/VoiceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Twilio.TwiML;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TwilioSampleInteg
+{
+    [Route("api/[controller]")]
+    public class VoiceController : Controller
+    {
+
+
+        [HttpPost]
+        public ActionResult Index()
+        {
+            var response = new VoiceResponse();
+            response.Say("Thanks for calling!");
+
+            return Content(response.ToString(), "text/xml");
+        }
+    }
+}
